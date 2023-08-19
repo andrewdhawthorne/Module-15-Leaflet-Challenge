60f2881fcfb3210967a12c535c4bd3c370954e4d
andrewdhawthorne <adhawthorne@gmail.com>
2023-08-19 12:41:33 -0500

    updating folder structure

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,38 +1,34 @@
 // Creating initial map object 
 var myMap = L.map("map", {
-    center: [39.8283, -98.5795],
-    zoom: 5
-  });
-
-  // Adding the tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
-  
-// Use this link to get the GeoJSON data
-let link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson?";
-  
-// Getting our GeoJSON data for all earthquakes in past day 
-d3.json(link).then(function(data) {
-  console.log(data);
-  // Creating a GeoJSON layer with the retrieved data
-  L.geoJson(data).addTo(myMap);
-}); 
-
-var earthquake = data.features; 
-
-features.forEach((id) => {
-
-  console.log(id); 
+  center: [39.8283, -98.5795],
+  zoom: 5
 });
 
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
 
+// Use this link to get the GeoJSON data
+let link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson?";
 
+// Getting our GeoJSON data for all earthquakes in past day 
+d3.json(link).then(function(data) {
+console.log(data);
+// Creating a GeoJSON layer with the retrieved data
+L.geoJson(data).addTo(myMap);
+}); 
 
+//let coordinates = data.features[0].geometry.coordinates; 
 
+//console.log(coordinates)
 
+//var earthquake = data.features; 
 
+//features.forEach((id) => {
 
+  //console.log(id); 
+//});
 
 // Define a markerSize() function that will give each earthquake a different radius based on its magnitude 
 //function markerSize(magnitude) {
